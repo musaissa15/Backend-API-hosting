957f97b2bba801a9ee913635ed3dfaacd13fab10
Musa Issa <musaissa@Musas-Air.broadband>
2022-05-18 16:38:57 +0100

    amendments made to t4

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const {
-	returnCategories,
-	returnReviews,
-	returnUpdatedReviews,
+  returnCategories,
+  returnReviews,
+  returnUpdatedReviews,
 } = require("./controller/controller");
 const app = express();
 
@@ -15,24 +15,28 @@ app.get("/api/reviews/:review_id", returnReviews);
 app.patch("/api/reviews/:review_id", returnUpdatedReviews);
 
 app.all("/*", (request, response, next) => {
-	response.status(404).send({ msg: "Invalid Path" });
+  response.status(404).send({ msg: "Invalid Path" });
 });
 
 app.use((error, request, response, next) => {
-	if (error.status && error.msg) {
-		response.status(error.status).send({ msg: error.msg });
-	} else if (error.code === "22P02") {
-		response.status(400).send({ msg: "Invalid input" });
-	} else next(error);
+  if (error.status && error.msg) {
+    response.status(error.status).send({ msg: error.msg });
+  } else if (error.code === "22P02") {
+    response.status(400).send({msg: "Invalid input"});
+   }
+  else next(error);
 });
 
-// app.use((error, request, response, next) => {
-//   response.status(404).send({msg: "invalid input"})
-//   next(error)
-// })
+app.use((error, request, response, next) => {
+	if (error.code === "23502") {
+		response.status(400).send({ msg: "Nothing inputed" });
+	} else {
+		next(error);
+	}
+});
 
 app.use((error, request, response, next) => {
-	response.status(500).send("Server Error!");
+  response.status(500).send("Server Error!");
 });
 
 module.exports = app;
--- controller/controller.js
+++ controller/controller.js
@@ -1,40 +1,39 @@
 const { response } = require("../app");
 const {
-	fetchCategories,
-	fetchReviews,
-	updateReviews,
+  fetchCategories,
+  fetchReviews,
+  updateReviews,
 } = require("../model/model");
 
 exports.returnCategories = (request, response, next) => {
-	fetchCategories()
-		.then((categories) => {
-			response.status(200).send({ categories });
-		})
-		.catch((error) => {
-			next(error);
-		});
+  fetchCategories()
+    .then((categories) => {
+      response.status(200).send({ categories });
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
 exports.returnReviews = (request, response, next) => {
-	const { review_id } = request.params;
-	fetchReviews(review_id)
-		.then((review) => {
-			response.status(200).send({ review });
-		})
-		.catch((error) => {
-			next(error);
-		});
+  const { review_id } = request.params;
+  fetchReviews(review_id)
+    .then((review) => {
+      response.status(200).send({ review });
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
 exports.returnUpdatedReviews = (request, response, next) => {
-	const { review_id } = request.params;
-  const {inc_votes} = request.body;
-	updateReviews(review_id, inc_votes)
-		.then((newReview) => {	
-    	response.status(200).send({review: newReview});
-		})
-		.catch((error) => {
-			console.log(error)
-			next(error)
-		})
+  const { review_id } = request.params;
+  const { inc_votes } = request.body;
+  updateReviews(review_id, inc_votes)
+    .then((newReview) => {
+      response.status(200).send({ review: newReview });
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
